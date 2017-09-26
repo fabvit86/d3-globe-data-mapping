413c85161bdc03cefced8c686c7c89a4be4a5fc6
Fabrizio Vitelli <fabvit86@yahoo.it>
2017-09-26 14:40:20 +0200

    added tooltip

--- app/js/graph.js
+++ app/js/graph.js
@@ -15,6 +15,11 @@ const svg = d3.select("#svgchart")
 
 const gMapContainer = svg.append("g").attr("class", "map")
 
+// tooltip div:
+const tooltip = d3.select('#mainContainer').append("div")
+	.classed("tooltip", true)
+	.style("opacity", 0) // start invisible
+
 const topoUrl = 'https://unpkg.com/world-atlas@1/world/110m.json'
 const meteoritesUrl = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json'
 
@@ -50,5 +55,29 @@ d3.json(topoUrl, (error, data) => {
 			.attr("class", "meteorite")
 			.attr("transform", d => "translate(" + path.centroid(d) + ")")
     	.attr("r", d => radius(d.properties.mass))
+    	.on("mouseover", function(d) {
+    		const properties = d.properties
+	    	d3.select(this).classed("overed", true) // add "overed" class to the rect
+	    	tooltip.transition()
+	    		.duration(300)
+	    		.style("opacity", 0.8) // show the tooltip
+	    	let tooltipContent = "<table><tbody>" +
+	    											"<tr><td>Landing site: </td><td><span class='property'>" + properties.name + "</span></td></tr>" +
+	    											"<tr><td>Mass: </td><td><span class='property'>" + d3.format(",")(+properties.mass) + "</span></td></tr>" +
+	    											"<tr><td>Year: </td><td><span class='property'>" + new Date(properties.year).getFullYear() + "</span></td></tr>" +
+	    											"<tr><td>Class: </td><td><span class='property'>" + properties.recclass + "</span></td></tr>" + 
+	    											"<tr><td>Latitude: </td><td><span class='property'>" + properties.reclat + "</span></td></tr>" + 
+	    											"<tr><td>Longitude: </td><td><span class='property'>" + properties.reclong + "</span></td></tr>" +
+	    											"</tbody></table>"
+	    	tooltip.html(tooltipContent)
+	      	.style("left", (d3.event.pageX - d3.select('.tooltip').node().offsetWidth / 2 - 5) + "px")
+	        .style("top", (d3.event.pageY - d3.select('.tooltip').node().offsetHeight) + "px");
+    	})
+    	.on("mouseleave", function(d) {
+    		d3.select(this).classed("overed", false)
+	    	tooltip.transition()
+	    		.duration(200)
+	    		.style("opacity", 0)
+    	})
 	})
 })
